108f778626c65af4924171fb84c0227674ac58e3
Jessica Ojo <jess@Jessicas-MacBook-Pro.local>
2023-03-18 11:21:19 +0100

    join scheduler jobs into one scheduler

--- ElectionMonitor/main.py
+++ ElectionMonitor/main.py
@@ -6,13 +6,17 @@ import tweepy as tp
 from apscheduler.schedulers.background import BackgroundScheduler
 
 dotenv.load_dotenv()
+dotenv_file = dotenv.find_dotenv()
 
 
 class ElectionMonitor:
     def __init__(self):
         print('initiating')
         # Authenticate API credentials
+        self.client_id = os.getenv('client_id')
         self.client = tp.Client(os.getenv('APP_ACCESS_TOKEN'))
+        self.client_secret = os.getenv('client_secret')
+        self.redirect_uri = os.getenv('redirect_uri')
         print('authentication done')
 
         self.keywords = ("Nigeria election 2023", "Nigerian politics", "INEC", "APC", "PDP",
@@ -37,15 +41,33 @@ class ElectionMonitor:
                     print('Retweeted:', tweet.id)
                     time.sleep(10)
 
-        # Save the updated list of retweeted ids
-        with open('retweeted_tweets.txt', 'w') as f:
-            f.write('\n'.join(str(tweet_id) for tweet_id in self.retweeted_tweets))
+                    # Save the updated list of retweeted ids
+                    with open('retweeted_tweets.txt', 'a') as f:
+                        f.write('\n'.join(str(tweet.id)))
+
+    def refresh_token(self):
+        auth = tp.OAuth2UserHandler(client_id=self.client_id,
+                                    client_secret=self.client_secret,
+                                    redirect_uri=self.redirect_uri,
+                                    scope=['tweet.read', 'tweet.write', 'tweet.moderate.write', 'users.read',
+                                           'follows.write', 'offline.access', 'space.read', 'like.write',
+                                           'bookmark.write'],
+                                    )
+
+        token_data = auth.refresh_token(
+            'https://api.twitter.com/2/oauth2/token', os.environ["REFRESH_TOKEN"]
+        )
+        os.environ['APP_ACCESS_TOKEN'] = token_data['access_token']
+        os.environ['REFRESH_TOKEN'] = token_data['refresh_token']
+        dotenv.set_key(dotenv_file, "APP_ACCESS_TOKEN", os.environ["APP_ACCESS_TOKEN"])
+        dotenv.set_key(dotenv_file, "REFRESH_TOKEN", os.environ["REFRESH_TOKEN"])
 
 
 if __name__ == '__main__':
     scheduler = BackgroundScheduler()
     election_monitor = ElectionMonitor()
-    scheduler.add_job(election_monitor.monitor(), 'interval', minutes=30)
+    scheduler.add_job(election_monitor.monitor, 'interval', minutes=30)
+    scheduler.add_job(election_monitor.refresh_token, 'interval', hours=1, minutes=55)
     scheduler.start()
 
     try:
